66e410c64cbcb8352a4ec970039e62de901143e7
PMathieu16 <mathieu.parinet@scub.net>
2023-10-27 19:20:28 +0200

    first try dnd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<script>
+	import DndZone from '$lib/dnd-zone.svelte';
+</script>
+
 <h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<DndZone />
